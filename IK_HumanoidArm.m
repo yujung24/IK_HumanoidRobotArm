4216285bd5beb059dfb52a9a362e5be80b17a07c
Yujung Jung <74299638+yujung24@users.noreply.github.com>
2024-09-13 22:27:56 +0900

    Add files via upload

--- IK_HumanoidArm.m
+++ IK_HumanoidArm.m
@@ -14,10 +14,13 @@ syms T s1 s2 s3 c1 c2 c3 % transfer
 
 q0 = [pi/4; pi/4; pi/4]; %initial angle : 512
 
+x=0.5; y=0.7; z=0.1;     % target coordinate (x,y,z)
 
-%% ============================ setting ============================
 
-%----------------------- Homogeneous Matrix -----------------------%
+%% ========================== setting ============================
+
+
+%---------------------- Homogeneous Matrix ----------------------%
 
 MT01 = DHmodified(0, pi/2, 0, 0);
 MT12 = DHmodified(0, 0, L1,  th1);
@@ -31,21 +34,17 @@ MT0e = simplify(MT01 * MT12 * MT23 * MT34 * MT45 * MT56 * MT6e);
 
 
 
-%--------------- calculate task space (Xdot = X1-X0) ---------------%
+%-------------- calculate task space (Xdot = X1-X0) --------------%
 
 % substitution theta
-X0 = [x0; y0; z0];
-% X0 = subs(MT0e(1:3, 4), th1, pi/4); % initial X : [x0, y0, z0]
-% X0 = subs(X0, th2, pi/4);
-% X0 = subs(X0, th3, pi/4);
+%X0 = [x0; y0; z0];
+X0 = subs(MT0e(1:3, 4), [th1;th2;th3], [pi/4;pi/4;pi/4]); % initial X : [x0, y0, z0]
 
-%X1 = MT0e(1:3, 4);   % target X : [x1, y1, z1]
-% OR
-X1 = [x1; y1; z1]; 
+X1 = [x1; y1; z1]; % target X
 
-Xdot = X1-X0;
+X_target = [x; y; z];
 
-%------------------------- Jacobian Matrix -------------------------%
+%------------------------ Jacobian Matrix ------------------------%
 
 % partial derivative
 for i = 1:3
@@ -62,8 +61,7 @@ lim1 = 0; lim2 = pi/2;      % 나중에 solve로 바꾸기
 
 
 
-
-%% =========================== first step ===========================
+%% ========================= first moving =========================
 
 J0 = subs(J,[th1;th2;th3],q0);
 
@@ -71,69 +69,46 @@ J0_inv = J0^(-1);
 q1 = J0*(X1-X0) + q0;
 
 
+%q_new = qdot + q_old;
 
-%q_new = qdot - q_old;
+X_old = X1;
+q_old = q1;
 
 
+X_new = subs(MT0e(1:3, 4), [th1; th2; th3], q_old);     % Joinst space -> Task space (FK)
+J_inv_old = subs(J_inv, [th1;th2;th3], q_old);
+q_new = J_inv_old*(X_new - X_old) + q_old;
 
 
-
-%% ======================== continuous moving ========================
+%% ====================== continuous moving =======================
 
 % FK : X0, -> q0 -> q1 -> J^(-1)
 
-q_new = simplify(J_inv*(X1-X0) + q_old);
+
+
 assume(th3 > 0 & th3 < pi/2); % singularity
 
-% Xdot, J_inv
 
-while(q_new==q_old)
-    q_new = qdot - q_old;
+
+while(X_target~=X_new)
+    
+    X_new = subs(MT0e(1:3, 4), [th1; th2; th3], q_old);     % Joinst space -> Task space (FK)
+    J_inv_old = subs(J_inv, [th1;th2;th3],q_old);
+    q_new = J_inv_old*(X_new - X_old) + q_old;
 
     if(th3 == lim1 | th3 == lim2)
+        break;
     end
 
 end
 
 
 
-%% =============================== plot ===============================
-
-J
-q0
-q1
-
-
-%====================== Substitution equation ======================%
-
-%q1 = subs(q1, L2*cos(th3)*sin(th2) - L3*sin(th3) + L4*cos(th3)*sin(th2), T);
-
-% substitution sin cos
-q1 = subs(q1, sin(th1), s1);
-q1 = subs(q1, sin(th2), s2);
-q1 = subs(q1, sin(th3), s3);
-q1 = subs(q1, cos(th1), c1);
-q1 = subs(q1, cos(th2), c2);
-q1 = subs(q1, cos(th3), c3);
-
-
-q1 = subs(q1, L2*c3*s2 - L3*s3 + L4*c3*s2, T);
-
-
-syms K K2 C C2 C3 C4    % C : Constant value
-q1 = subs(q1, (L2*c1 + L4*c1 + L3*s1 + c3*s1*s2*L5 + L5*c1*s3)*c2*s3, K);
-q1 = subs(q1, L2*s1 - L3*c1 + L4*s1 + L5*s1*s3 - L5*c1*c3*s2, K2);
-q1 = subs(q1, (2^(1/2)*L2)/2 + (2^(1/2)*L3)/2 + (2^(1/2)*L4)/2 + (2^(1/2)*L5)/4, C);
-q1 = subs(q1, (2^(1/2)*L2)/2 - (2^(1/2)*L3)/2 + (2^(1/2)*L4)/2 - (2^(1/2)*L5)/4 + L5/2 - z1, C2);
-q1 = subs(q1, L1 + L5/2 + y1, C3);
-q1 = subs(q1, C + L5/2 - x1, C4);
+%% ============================== plot ==============================
 
 
 
 
-%div = simplify(q1(2)/q1(1))
-
-%simplify(atan2(q1(2), q1(1)))
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
